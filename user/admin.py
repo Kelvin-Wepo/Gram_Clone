8cfda303f35d1ad6ebcd7d7785bf18d133fbc5d8
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-06-08 19:08:14 +0300

    feat: this commit modifies app user

---
+++ user/admin.py
@@ -0,0 +1,95 @@
+"""User admin classes."""
+
+# Django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib import admin
+
+
+# models
+from django.contrib.auth.models import User
+from user.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Profile Admin"""
+    # List of attributes that will show in the admin
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    # List of links that lead to detail
+    list_display_links = ('pk', 'user')
+    # List of editable in situ
+    list_editable = ('phone_number', 'website', 'picture')
+    #Searchable Fields
+    search_fields = (
+        'user__email',
+        'user__username',
+        'user__first_name',
+        'user__last_name',
+        'phone_number'
+    )
+    
+    # Fields to filter by 
+    list_filter = (
+        'user__is_active',
+        'user__is_staff',
+        'created',
+        'modified',
+    )
+    # group fields
+    fieldsets = (
+        (
+            'Profile',
+            {
+                # To arrange them horizontally you can do
+                # placing a turple inside another turple and comma
+                # (('user', 'picture'),)
+                'fields': (('user', 'picture'),)
+            }
+        ),
+
+        # If we don't want it to appear
+        # the blue title bar we can pass None
+        (
+            'Extra info',
+            {
+                'fields': (
+                    ('phone_number', 'website'),
+                    ('biography'),
+                )
+            }
+        ),
+        (
+            'Metadata',
+            {
+                'fields': (('created', 'modified'),)
+            }
+        ),
+    )
+    readonly_fields = ('created', 'modified')
+
+# Para que ambos admins se vean 
+# So that both admins are seen in one, it is done in the following way
+
+
+class ProfileInline(admin.StackedInline):
+        """Profile in-line admin for users."""
+        model = Profile
+        can_delete = False
+        verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin."""
+    inlines = (ProfileInline, )
+    list_display = (
+        'username',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff',
+    )
+
+
+admin.site.unregister(User)
+# admin.site.register(Modelo,Clase)
+admin.site.register(User, UserAdmin)
---
+++ user/models.py
@@ -0,0 +1,45 @@
+# User models
+
+# Django
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    """
+    Profile model
+
+    Proxy Model that extends the base data with other information.
+    """
+    
+    # Relationship with the user table
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # extended fields
+
+    website = models.URLField(max_length=200, blank=True)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    picture = models.ImageField(
+        upload_to='user/pictures',
+        blank=True,
+        null=True
+    )
+
+    # time
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    likes = models.ManyToManyField(User, related_name='blogpost_like')
+
+
+    def __str__(self):
+        """Return username"""
+        return self.user.username
+
+    def number_of_likes(self):
+        return self.likes.count()
